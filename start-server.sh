39fb4b6d5505cff289ac56a8535e33a7cac030a4
Ming Tsai <mingster.tsai@gmail.com>
2024-04-05 10:19:47 +0800

    Update start-server.sh

--- start-server.sh
+++ start-server.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-source ./setenv.sh
-cd $JELLYFIN_SERVER_DIR
 
-echo 'build jellyfin-server'
-dotnet build
+source ./setenv.sh
+
+if [ ! -f "$JELLYFIN_SERVER_DIR/Jellyfin.Server/bin/Debug/net8.0/jellyfin" ]
+  then
+  echo 'build jellyfin-server'
+  cd $JELLYFIN_SERVER_DIR
+  dotnet build
+fi;
 
 echo 'run jellyfin-server'
 $JELLYFIN_SERVER_DIR/Jellyfin.Server/bin/Debug/net8.0/jellyfin
